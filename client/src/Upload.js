5ca5aa71c43ff4eea842bbb62e3e7483a6455d8c
kugaitomomi <tottomo7@gmail.com>
2022-08-21 18:20:36 +0900

    connect knex and postgresql

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -5,8 +5,10 @@ function Upload() {
 
   const [pic, setPic] = useState();
   const [allPics, setAllPics] = useState([]);
+  const [ category, setCategory ] = useState();
+  const [ note, setNote ] = useState();
   useEffect(() => {
-    getAllPics();
+    getAllInfo();
   },[allPics]);
 
     const handleSubmit = async (e) => {
@@ -16,7 +18,9 @@ function Upload() {
     //useStateで取得しているサーバーに格納されている情報
     console.log(pic);
     formData.append("pic", pic);
-    await axios.post("http://localhost:5000/addPicture", formData).then(getAllPics()).catch((error) => console.log(error.message));
+    formData.append("category", category);
+    formData.append("note", note);
+    await axios.post("http://localhost:5000/addPicture", formData).then(getAllInfo()).catch((error) => console.log(error));
   }
 
     const handleChange = (e) => {
@@ -24,7 +28,7 @@ function Upload() {
     console.log(e.target.files[0]);
   }
 
-  const getAllPics = async () => {
+  const getAllInfo = async () => {
     await axios.get('http://localhost:5000/pictures').then(res => {
       setAllPics(res.data);
       console.log(allPics);
@@ -39,20 +43,37 @@ function Upload() {
         //bodyに値をセットする場合は、第2引数にdataというキー名でセットする。
         data: { name: name },
       })
-      .then(getAllPics())
+      .then(getAllInfo())
       .catch((error) => console.log(error.message));
   };
+
+  const selectChange = (e) => {
+    setCategory(e.target.value);
+  }
+
+  const insertChange = (e) => {
+    setNote(e.target.value);
+  }
   
 return (
   <>
   {/* JavaScriptのonsubmitとは、送信ボタンが押された時に起動するイベントです。 onClickと混同しない！ */}
     <form onSubmit={handleSubmit}>
       <input type="file" className="input" onChange={handleChange} />
+      <select name="example" value={category} onChange={selectChange}>
+      <option value="Daily">Daily</option>
+      <option value="Special">Special</option>
+      <option value="Date">Date</option>
+      <option value="Home">Home</option>
+      <option value="メルカリ">メルカリ</option>
+      <option value="ラクマ">ラクマ</option>
+      </select>
+      <input type="text" value={note} onChange={insertChange} />
       <button className="submit" >upload</button>
     </form>
     <div className="imgsContainer">
       {allPics && allPics.map((pic, index) => {
-        return <div className="imgItem" key={index}><img src={pic.url} alt={pic.name} width="50%" /><button className="imgButton" onClick={() => handleDelete(pic.name)}>Delete</button></div>
+        return <div className="imgItem" key={index}><img src={pic.url} alt={pic.name} width="50%" /><p>{pic.category}</p><p>{pic.note}</p><button className="imgButton" onClick={() => handleDelete(pic.name)}>Delete</button></div>
         //onClickで「関数名()」だとクリックしていなくても、関数が実行されてしまうので削除系の関数の場合は、値が削除されてしまいデータがなくなってしまう。なので、無名関数を挟むことで、「クリックされた時」というタイミングで関数実行ができるようになる。
       })}
       
--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+//knexとpsqlで作成したDBを繋げるコード↓
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "localhost",
+    port: 5432,
+    database: "fashionmemo",
+    user: "tomomi",
+  }
+});
+
 const {
   ref,
   uploadBytes,
@@ -22,12 +33,17 @@ app.post("/addPicture", upload.single("pic"),async (req, res) => {
   const file = req.file;
   const imageRef = ref(storage, file.originalname);
   const metatype = { contentType: file.mimetype, name: file.originalname };
-  await uploadBytes(imageRef, file.buffer, metatype).then((snapshot) => {
-    console.log(imageRef);
-    console.log(metatype);
-      res.send("uploaded!");
-    })
-    .catch((err) => console.error(err.message));
+  console.log(file.originalname);
+  //最初に try ブロック内のコードが実行され、例外がスローされた場合は catch ブロック内のコードが実行されます。
+  try {
+    await uploadBytes(imageRef, file.buffer, metatype);
+    await knex("fashion").insert({ name: file.originalname , category: req.body.category, note: req.body.note });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
+
+  res.status(200).send();
 });
 
 //get all picture
@@ -36,19 +52,25 @@ app.get("/pictures", async(req, res) => {
   const listRef = ref(storage);
 
   //↓↓配列にして、map関数で一つ一つ取り出してデータをJson形式で表示している。
-  let prductPictures = [];
-  await listAll(listRef).then((pics) => {
-    prductPictures= pics.items.map((item) => {
+  const productPictures = [];
+  await listAll(listRef).then(async (pics) => {
+  for(const item of pics.items){
       const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${item._location.bucket}/o/${item._location.path_}?alt=media`;
-      console.log(listRef);
-      return {
+      const serachResult = await knex.select("category", "note").from("fashion").where("name", item.name).first();
+      const category = serachResult?.category;
+      const note = serachResult?.note;
+      console.log(item.name);
+      console.log(serachResult);
+      productPictures.push({
         url: publicUrl,
         name: item._location.path_,
-      };
-    });
-    res.send(prductPictures);
+        category,
+        note,
+      });
+  };
+    res.status(200).send(productPictures);
   })
-  .catch((err) => console.error(err.message));
+  .catch((err) => console.error(err));
 })
 
 //delete a picture
@@ -56,9 +78,14 @@ app.delete("/delete", async(req, res) => {
   const deletePic = req.body.name;
   const deleteRef = ref(storage, deletePic);
   //「deleteObject()」メソッドはfirebase側のdelete() メソッド
-  await deleteObject(deleteRef).then(() => {
-    res.send("deleted");
-  }).catch((err) => console.error(err.message));
+  try {
+     await deleteObject(deleteRef);
+     await knex("fashion").where("name", deletePic).del();
+     res.status(200).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 });
 
 
